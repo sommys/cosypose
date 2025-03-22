1aa76aee128fc5aa2b11db336a6886982558e7a8
Soma Seres <somaseres@gmail.com>
2025-03-22 23:46:21 +0100

    chore: Modify setup.py to enable installing the module with pip and allow building the CppExtensions as well

--- setup.py
+++ setup.py
@@ -1,17 +1,32 @@
-import os
-from setuptools import setup
-from torch.utils.cpp_extension import BuildExtension, CppExtension
+from os import path, environ
+from setuptools import setup, find_packages
 
-os.environ['CXX'] = os.environ.get('GXX', '')
+here = path.abspath(path.dirname(__file__))
+
+environ["CXX"] = environ.get("GXX", "g++")
+
+try:
+    from torch.utils.cpp_extension import BuildExtension, CppExtension
+
+    ext_modules = [
+        CppExtension(
+            name="cosypose_cext",
+            sources=["cosypose/csrc/cosypose_cext.cpp"],
+            extra_compile_args=["-O3"],
+            verbose=True,
+        )
+    ]
+    cmdclass = {"build_ext": BuildExtension}
+except ImportError:
+    print("Warning: torch is not installed in the installer venv. Skipping C++ extension build.")
+    ext_modules = []
+    cmdclass = {}
 
 setup(
-    ext_modules=[
-        CppExtension(
-            name='cosypose_cext',
-            sources=['cosypose/csrc/cosypose_cext.cpp'],
-            extra_compile_args=['-O3'],
-            verbose=True
-        )
-    ],
-    cmdclass={'build_ext': BuildExtension}
+    name="cosypose",
+    version="1.0.0",
+    description="CosyPose",
+    packages=find_packages(),
+    ext_modules=ext_modules,
+    cmdclass=cmdclass,
 )
