35862476f69bd0336624d01765ccd71709d7a33e
Soma Seres <somaseres@gmail.com>
2025-03-28 12:50:38 +0100

    chore: Applied black formatting

--- cosypose/models/pose.py
+++ cosypose/models/pose.py
@@ -8,17 +8,18 @@ from cosypose.lib3d.cropping import deepim_crops_robust as deepim_crops
 from cosypose.lib3d.camera_geometry import project_points_robust as project_points
 
 from cosypose.lib3d.rotations import (
-    compute_rotation_matrix_from_ortho6d, compute_rotation_matrix_from_quaternions)
+    compute_rotation_matrix_from_ortho6d,
+    compute_rotation_matrix_from_quaternions,
+)
 from cosypose.lib3d.cosypose_ops import apply_imagespace_predictions
 
 from cosypose.utils.logging import get_logger
+
 logger = get_logger(__name__)
 
 
 class PosePredictor(nn.Module):
-    def __init__(self, backbone, renderer,
-                 mesh_db, render_size=(240, 320),
-                 pose_dim=9):
+    def __init__(self, backbone, renderer, mesh_db, render_size=(240, 320), pose_dim=9):
         super().__init__()
 
         self.backbone = backbone
@@ -31,7 +32,7 @@ class PosePredictor(nn.Module):
 
         self.heads = dict()
         self.pose_fc = nn.Linear(n_features, pose_dim, bias=True)
-        self.heads['pose'] = self.pose_fc
+        self.heads["pose"] = self.pose_fc
 
         self.debug = False
         self.tmp_debug = dict()
@@ -52,15 +53,26 @@ class PosePredictor(nn.Module):
         uv = project_points(points, K, TCO)
         boxes_rend = boxes_from_uv(uv)
         boxes_crop, images_cropped = deepim_crops(
-            images=images, obs_boxes=boxes_rend, K=K,
-            TCO_pred=TCO, O_vertices=points, output_size=self.render_size, lamb=1.4
+            images=images,
+            obs_boxes=boxes_rend,
+            K=K,
+            TCO_pred=TCO,
+            O_vertices=points,
+            output_size=self.render_size,
+            lamb=1.4,
         )
-        K_crop = get_K_crop_resize(K=K.clone(), boxes=boxes_crop,
-                                   orig_size=images.shape[-2:], crop_resize=self.render_size)
+        K_crop = get_K_crop_resize(
+            K=K.clone(),
+            boxes=boxes_crop,
+            orig_size=images.shape[-2:],
+            crop_resize=self.render_size,
+        )
         if self.debug:
             self.tmp_debug.update(
                 boxes_rend=boxes_rend,
-                rend_center_uv=project_points(torch.zeros(bsz, 1, 3).to(K.device), K, TCO),
+                rend_center_uv=project_points(
+                    torch.zeros(bsz, 1, 3).to(K.device), K, TCO
+                ),
                 uv=uv,
                 boxes_crop=boxes_crop,
             )
@@ -74,7 +86,7 @@ class PosePredictor(nn.Module):
             dR = compute_rotation_matrix_from_quaternions(pose_outputs[:, 0:4])
             vxvyvz = pose_outputs[:, 4:7]
         else:
-            raise ValueError(f'pose_dim={self.pose_dim} not supported')
+            raise ValueError(f"pose_dim={self.pose_dim} not supported")
         TCO_updated = apply_imagespace_predictions(TCO, K_crop, vxvyvz, dR)
         return TCO_updated
 
@@ -96,37 +108,42 @@ class PosePredictor(nn.Module):
         TCO_input = TCO
         for n in range(n_iterations):
             TCO_input = TCO_input.detach()
-            images_crop, K_crop, boxes_rend, boxes_crop = self.crop_inputs(images, K, TCO_input, labels)
-            renders = self.renderer.render(obj_infos=[dict(name=l) for l in labels],
-                                           TCO=TCO_input,
-                                           K=K_crop, resolution=self.render_size)
+            images_crop, K_crop, boxes_rend, boxes_crop = self.crop_inputs(
+                images, K, TCO_input, labels
+            )
+            renders = self.renderer.render(
+                obj_infos=[dict(name=l) for l in labels],
+                TCO=TCO_input,
+                K=K_crop,
+                resolution=self.render_size,
+            )
 
             x = torch.cat((images_crop, renders), dim=1)
 
             model_outputs = self.net_forward(x)
 
-            TCO_output = self.update_pose(TCO_input, K_crop, model_outputs['pose'])
+            TCO_output = self.update_pose(TCO_input, K_crop, model_outputs["pose"])
 
-            outputs[f'iteration={n+1}'] = {
-                'TCO_input': TCO_input,
-                'TCO_output': TCO_output,
-                'K_crop': K_crop,
-                'model_outputs': model_outputs,
-                'boxes_rend': boxes_rend,
-                'boxes_crop': boxes_crop,
+            outputs[f"iteration={n+1}"] = {
+                "TCO_input": TCO_input,
+                "TCO_output": TCO_output,
+                "K_crop": K_crop,
+                "model_outputs": model_outputs,
+                "boxes_rend": boxes_rend,
+                "boxes_crop": boxes_crop,
             }
 
             TCO_input = TCO_output
 
             if self.debug:
-                self.tmp_debug.update(outputs[f'iteration={n+1}'])
+                self.tmp_debug.update(outputs[f"iteration={n+1}"])
                 self.tmp_debug.update(
                     images=images,
                     images_crop=images_crop,
                     renders=renders,
                 )
-                path = DEBUG_DATA_DIR / f'debug_iter={n+1}.pth.tar'
-                logger.info(f'Wrote debug data: {path}')
+                path = DEBUG_DATA_DIR / f"debug_iter={n+1}.pth.tar"
+                logger.info(f"Wrote debug data: {path}")
                 torch.save(self.tmp_debug, path)
 
         return outputs
