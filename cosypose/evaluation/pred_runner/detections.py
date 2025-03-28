d0fd89781932044bea08484a135a0f49b7a88694
Soma Seres <somaseres@gmail.com>
2025-03-28 12:44:16 +0100

    fix: Fix xarray related errors by using the 'data' property where needed (+ applied black formatting)

--- cosypose/evaluation/pred_runner/detections.py
+++ cosypose/evaluation/pred_runner/detections.py
@@ -17,11 +17,17 @@ class DetectionRunner:
         self.world_size = get_world_size()
         self.tmp_dir = get_tmp_dir()
 
-        sampler = DistributedSceneSampler(scene_ds, num_replicas=self.world_size, rank=self.rank)
+        sampler = DistributedSceneSampler(
+            scene_ds, num_replicas=self.world_size, rank=self.rank
+        )
         self.sampler = sampler
-        dataloader = DataLoader(scene_ds, batch_size=batch_size,
-                                num_workers=n_workers,
-                                sampler=sampler, collate_fn=self.collate_fn)
+        dataloader = DataLoader(
+            scene_ds,
+            batch_size=batch_size,
+            num_workers=n_workers,
+            sampler=sampler,
+            collate_fn=self.collate_fn,
+        )
 
         if cache_data:
             self.dataloader = list(tqdm(dataloader))
@@ -36,19 +42,19 @@ class DetectionRunner:
         for n, data in enumerate(batch):
             rgb, masks, obs = data
             batch_im_id += 1
-            frame_info = obs['frame_info']
-            im_info = {k: frame_info[k] for k in ('scene_id', 'view_id')}
+            frame_info = obs["frame_info"]
+            im_info = {k: frame_info[k] for k in ("scene_id", "view_id")}
             im_info.update(batch_im_id=batch_im_id)
             im_infos.append(im_info)
             images.append(rgb)
 
-            for o, obj in enumerate(obs['objects']):
+            for o, obj in enumerate(obs["objects"]):
                 obj_info = dict(
-                    label=obj['name'],
+                    label=obj["name"],
                     score=1.0,
                 )
                 obj_info.update(im_info)
-                bboxes.append(obj['bbox'])
+                bboxes.append(obj["bbox"])
                 det_infos.append(obj_info)
 
         gt_detections = tc.PandasTensorCollection(
@@ -62,28 +68,28 @@ class DetectionRunner:
         )
         return data
 
-    def get_predictions(self,
-                        detector,
-                        gt_detections=False):
+    def get_predictions(self, detector, gt_detections=False):
 
         predictions = defaultdict(list)
 
         for data in tqdm(self.dataloader):
-            images = data['images'].cuda().float().permute(0, 3, 1, 2) / 255
+            images = data["images"].cuda().float().permute(0, 3, 1, 2) / 255
 
             if gt_detections:
-                preds = data['gt_detections']
+                preds = data["gt_detections"]
             else:
                 preds = detector.get_detections(
                     images=images,
                     one_instance_per_class=False,
                 )
 
-            im_infos = data['im_infos']
-            for k in ('scene_id', 'view_id'):
-                preds.infos[k] = preds.infos['batch_im_id'].apply(lambda idx: im_infos[idx][k])
+            im_infos = data["im_infos"]
+            for k in ("scene_id", "view_id"):
+                preds.infos[k] = preds.infos["batch_im_id"].apply(
+                    lambda idx: im_infos[idx][k]
+                )
 
-            predictions['detections'].append(preds)
+            predictions["detections"].append(preds)
 
         for k, v in predictions.items():
             predictions[k] = tc.concatenate(predictions[k])
