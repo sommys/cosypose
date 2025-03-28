f24746c34da051bb8e81f7b494b942f47c3f1297
Soma Seres <somaseres@gmail.com>
2025-03-28 12:55:12 +0100

    fix: Use UnsafeLoader to support loading Path objects from the config and add ballvalve detector (initial) (+ applied black formatting)

--- cosypose/scripts/run_detection_eval.py
+++ cosypose/scripts/run_detection_eval.py
@@ -11,10 +11,16 @@ from cosypose.datasets.bop import BOPDataset, remap_bop_targets
 
 from cosypose.evaluation.runner_utils import format_results
 
-from cosypose.training.detector_models_cfg import create_model_detector, check_update_config
+from cosypose.training.detector_models_cfg import (
+    create_model_detector,
+    check_update_config,
+)
 from cosypose.integrated.detector import Detector
 from cosypose.evaluation.pred_runner.detections import DetectionRunner
-from cosypose.scripts.run_cosypose_eval import load_pix2pose_results, load_posecnn_results
+from cosypose.scripts.run_cosypose_eval import (
+    load_pix2pose_results,
+    load_posecnn_results,
+)
 
 from cosypose.evaluation.meters.detection_meters import DetectionMeter
 from cosypose.evaluation.eval_runner.detection_eval import DetectionEvaluation
@@ -26,7 +32,8 @@ from cosypose.config import BOP_DS_DIR, EXP_DIR, RESULTS_DIR
 from cosypose.utils.logging import get_logger
 
 import torch.multiprocessing
-torch.multiprocessing.set_sharing_strategy('file_system')
+
+torch.multiprocessing.set_sharing_strategy("file_system")
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
@@ -35,12 +42,12 @@ logger = get_logger(__name__)
 
 def load_detector(run_id):
     run_dir = EXP_DIR / run_id
-    cfg = yaml.load((run_dir / 'config.yaml').read_text(), Loader=yaml.FullLoader)
+    cfg = yaml.load((run_dir / "config.yaml").read_text(), Loader=yaml.UnsafeLoader)
     cfg = check_update_config(cfg)
     label_to_category_id = cfg.label_to_category_id
     model = create_model_detector(cfg, len(label_to_category_id))
-    ckpt = torch.load(run_dir / 'checkpoint.pth.tar')
-    ckpt = ckpt['state_dict']
+    ckpt = torch.load(run_dir / "checkpoint.pth.tar")
+    ckpt = ckpt["state_dict"]
     model.load_state_dict(ckpt)
     model = model.cuda().eval()
     model.cfg = cfg
@@ -53,8 +60,8 @@ def get_meters(scene_ds):
     visib_gt_min = -1
     n_top = -1  # Given by targets
 
-    if 'bop19' in scene_ds.name:
-        targets_filename = 'test_targets_bop19.json'
+    if "bop19" in scene_ds.name:
+        targets_filename = "test_targets_bop19.json"
         targets_path = scene_ds.ds_dir / targets_filename
         targets = pd.read_json(targets_path)
         targets = remap_bop_targets(targets)
@@ -70,13 +77,13 @@ def get_meters(scene_ds):
     )
 
     meters = {
-        'ntop=BOP': DetectionMeter(**base_kwargs, consider_all_predictions=False),
-        'ntop=ALL': DetectionMeter(**base_kwargs, consider_all_predictions=True),
+        "ntop=BOP": DetectionMeter(**base_kwargs, consider_all_predictions=False),
+        "ntop=ALL": DetectionMeter(**base_kwargs, consider_all_predictions=True),
     }
     return meters
 
 
-def run_detection_eval(args, detector=None, split='test'):
+def run_detection_eval(args, detector=None, split="test"):
     logger.info(f"{'-'*80}")
     for k, v in args.__dict__.items():
         logger.info(f"{k}: {v}")
@@ -85,9 +92,12 @@ def run_detection_eval(args, detector=None, split='test'):
     scene_ds = scene_ds = BOPDataset(BOP_DS_DIR / args.ds_name, split=split)
 
     pred_kwargs = dict()
-    pred_runner = DetectionRunner(scene_ds, batch_size=args.pred_bsz,
-                                  cache_data=len(pred_kwargs) > 1,
-                                  n_workers=args.n_workers)
+    pred_runner = DetectionRunner(
+        scene_ds,
+        batch_size=args.pred_bsz,
+        cache_data=len(pred_kwargs) > 1,
+        n_workers=args.n_workers,
+    )
 
     if not args.skip_model_predictions:
         if detector is not None:
@@ -95,20 +105,17 @@ def run_detection_eval(args, detector=None, split='test'):
         else:
             model = load_detector(args.detector_run_id)
 
-        pred_kwargs.update({
-            'model': dict(
-                detector=model,
-                gt_detections=False
-            )
-        })
+        pred_kwargs.update({"model": dict(detector=model, gt_detections=False)})
 
     all_predictions = dict()
 
     if args.external_predictions:
-        if 'ycbv' in args.ds_name:
-            all_predictions['posecnn'] = load_posecnn_results().cpu()
-        elif 'tless' in args.ds_name:
-            all_predictions['retinanet/pix2pose'] = load_pix2pose_results(all_detections=True).cpu()
+        if "ycbv" in args.ds_name:
+            all_predictions["posecnn"] = load_posecnn_results().cpu()
+        elif "tless" in args.ds_name:
+            all_predictions["retinanet/pix2pose"] = load_pix2pose_results(
+                all_detections=True
+            ).cpu()
         else:
             pass
 
@@ -116,7 +123,7 @@ def run_detection_eval(args, detector=None, split='test'):
         logger.info(f"Prediction: {pred_prefix}")
         preds = pred_runner.get_predictions(**pred_kwargs_n)
         for preds_name, preds_n in preds.items():
-            all_predictions[f'{pred_prefix}/{preds_name}'] = preds_n
+            all_predictions[f"{pred_prefix}/{preds_name}"] = preds_n
 
     logger.info("Done with predictions")
     torch.distributed.barrier()
@@ -124,10 +131,14 @@ def run_detection_eval(args, detector=None, split='test'):
     # Evaluation.
     meters = get_meters(scene_ds)
     logger.info(f"Meters: {meters}")
-    eval_runner = DetectionEvaluation(scene_ds, meters, batch_size=args.eval_bsz,
-                                      cache_data=len(all_predictions) > 1,
-                                      n_workers=args.n_workers,
-                                      sampler=pred_runner.sampler)
+    eval_runner = DetectionEvaluation(
+        scene_ds,
+        meters,
+        batch_size=args.eval_bsz,
+        cache_data=len(all_predictions) > 1,
+        n_workers=args.n_workers,
+        sampler=pred_runner.sampler,
+    )
 
     eval_metrics, eval_dfs = dict(), dict()
     if not args.skip_evaluation:
@@ -148,12 +159,12 @@ def run_detection_eval(args, detector=None, split='test'):
     if get_rank() == 0:
         save_dir = Path(args.save_dir)
         save_dir.mkdir(exist_ok=True, parents=True)
-        logger.info(f'Finished evaluation on {args.ds_name}')
+        logger.info(f"Finished evaluation on {args.ds_name}")
         results = format_results(all_predictions, eval_metrics, eval_dfs)
-        torch.save(results, save_dir / 'results.pth.tar')
-        (save_dir / 'summary.txt').write_text(results.get('summary_txt', ''))
-        (save_dir / 'config.yaml').write_text(yaml.dump(args))
-        logger.info(f'Saved predictions+metrics in {save_dir}')
+        torch.save(results, save_dir / "results.pth.tar")
+        (save_dir / "summary.txt").write_text(results.get("summary_txt", ""))
+        (save_dir / "config.yaml").write_text(yaml.dump(args))
+        logger.info(f"Saved predictions+metrics in {save_dir}")
 
     logger.info("Done with evaluation")
     torch.distributed.barrier()
@@ -163,22 +174,22 @@ def run_detection_eval(args, detector=None, split='test'):
 def main():
     loggers = [logging.getLogger(name) for name in logging.root.manager.loggerDict]
     for logger in loggers:
-        if 'cosypose' in logger.name:
+        if "cosypose" in logger.name:
             logger.setLevel(logging.DEBUG)
 
-    parser = argparse.ArgumentParser('Evaluation')
-    parser.add_argument('--debug', action='store_true')
-    parser.add_argument('--skip_predictions', action='store_true')
-    parser.add_argument('--comment', default='', type=str)
-    parser.add_argument('--id', default=-1, type=int)
-    parser.add_argument('--config', default='', type=str)
-    parser.add_argument('--models', default='', type=str)
+    parser = argparse.ArgumentParser("Evaluation")
+    parser.add_argument("--debug", action="store_true")
+    parser.add_argument("--skip_predictions", action="store_true")
+    parser.add_argument("--comment", default="", type=str)
+    parser.add_argument("--id", default=-1, type=int)
+    parser.add_argument("--config", default="", type=str)
+    parser.add_argument("--models", default="", type=str)
     args = parser.parse_args()
 
     device = torch.cuda.current_device()
     init_distributed_mode(device=device)
 
-    cfg = argparse.ArgumentParser('').parse_args([])
+    cfg = argparse.ArgumentParser("").parse_args([])
 
     cfg.n_workers = 8
     cfg.pred_bsz = 8
@@ -191,38 +202,35 @@ def main():
     if args.debug:
         cfg.n_frames = 10
 
-    if args.config == 'bop':
-        # ds_names = ['ycbv.bop19', 'tless.bop19']
-        ds_names = ['itodd.val', 'hb.val']
-    else:
-        raise ValueError
+    ds_names = ["ballvalve"]
 
     detector_run_ids = {
-        'ycbv.bop19': 'ycbv--377940',
-        'hb.val': 'detector-bop-hb--497808',
-        'itodd.val': 'detector-bop-itodd--509908',
+        "ycbv.bop19": "ycbv--377940",
+        "hb.val": "detector-bop-hb--497808",
+        "itodd.val": "detector-bop-itodd--509908",
+        "ballvalve": "detector-ballvalve-2025_03_24_14_13_10",
     }
 
     if args.id < 0:
         n_rand = np.random.randint(int(1e6))
         args.id = n_rand
-    save_dir = RESULTS_DIR / f'{args.config}-{args.models}-{args.comment}-{args.id}'
-    logger.info(f'Save dir: {save_dir}')
+    save_dir = RESULTS_DIR / f"{args.config}-{args.models}-{args.comment}-{args.id}"
+    logger.info(f"Save dir: {save_dir}")
 
     for ds_name in ds_names:
         this_cfg = deepcopy(cfg)
         this_cfg.ds_name = ds_name
-        this_cfg.save_dir = save_dir / f'dataset={ds_name}'
-        logger.info(f'DATASET: {ds_name}')
+        this_cfg.save_dir = save_dir / f"dataset={ds_name}"
+        logger.info(f"DATASET: {ds_name}")
 
         if ds_name in detector_run_ids:
             this_cfg.detector_run_id = detector_run_ids[ds_name]
         else:
             this_cfg.skip_model_predictions = True
-            logger.info(f'No model provided for dataset: {ds_name}.')
+            logger.info(f"No model provided for dataset: {ds_name}.")
         run_detection_eval(this_cfg)
-        logger.info('')
+        logger.info("")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
