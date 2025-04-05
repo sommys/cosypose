adf4c9f6f33803cfc62829ac033e9b8c738eb366
Soma Seres <somaseres@gmail.com>
2025-04-05 20:20:55 +0200

    fix: use correct backbone config for detector after refactoring

--- cosypose/training/detector_models_cfg.py
+++ cosypose/training/detector_models_cfg.py
@@ -9,8 +9,10 @@ def check_update_config(cfg):
 
 
 def create_model_detector(cfg, n_classes):
-    model = DetectorMaskRCNN(input_resize=cfg.input_resize,
-                             n_classes=n_classes,
-                             backbone_str=cfg.backbone_str,
-                             anchor_sizes=cfg.anchor_sizes)
+    model = DetectorMaskRCNN(
+        input_resize=cfg.input_resize,
+        n_classes=n_classes,
+        backbone_str=cfg.backbone,
+        anchor_sizes=cfg.anchor_sizes,
+    )
     return model
